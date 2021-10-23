3a4155f320384c183213e5f081f718b49724e44d
Kambar-ZH <zhamauov02@mail.ru>
2021-10-23 20:36:31 +0600

    changes for v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test
+
+go 1.16
+
+require github.com/Kambar-ZH/Golang_Onelab v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Kambar-ZH/Golang_Onelab/utils"
+)
+
+func main() {
+	fmt.Println(strings.Lower("This is the first part"))
+	fmt.Println(strings.Upper("This is the second second"))
+}
